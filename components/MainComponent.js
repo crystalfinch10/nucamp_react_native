8988abed210e820176a0f06f66c3d01b81c028d5
finchy10 <crystalfinch11@gmail.com>
2020-12-12 16:18:35 -0700

    workshop 4

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -325,13 +325,17 @@ class Main extends Component {
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
+        this.showNetInfo();
+    }
 
+    showNetInfo = async () => {
+        const connectionInfo = await NetInfo.fetch();
+        
+        (Platform.OS === 'ios') ?
+            Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+            : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                connectionInfo.type, ToastAndroid.LONG);
+        
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
         });
@@ -357,8 +361,7 @@ class Main extends Component {
                 connectionMsg = 'You are now connected to a WiFi network.';
                 break;
         }
-        (Platform.OS === 'ios')
-            ? Alert.alert('Connection change:', connectionMsg)
+        (Platform.OS === 'ios') ? Alert.alert('Connection change:', connectionMsg)
             : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
     }
     
